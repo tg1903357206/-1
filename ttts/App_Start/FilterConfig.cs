e9b3da8fda64e9c94033bb0f1e184ab4dd07b372
tg1903357206 <1903357206@qq.com>
2019-01-10 14:34:28 +0800

    tji

---
+++ ttts/App_Start/FilterConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR.Hosting;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ttts
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ttts/Controllers/EasyuiController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ttts.Controllers
+{
+    public class EasyuiController : Controller
+    {
+        // GET: Easyui
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Table()
+        {
+            return View();
+        }
+
+        public ActionResult table2()
+        {
+            return View();
+        }
+    }
+}
